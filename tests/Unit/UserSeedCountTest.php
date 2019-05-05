bfa7c9a90682b7b85b7640bc34ed8bbcbd4e4b5a
gdp9 <gdp9@njit.edu>
2019-05-05 18:34:11 -0400

    Unit Test#8: Create a unit test to test to count the number of records inserted by the database seed.  i.e. $carCount = 50

---
+++ tests/Unit/UserSeedCountTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserSeedCountTest extends TestCase
+{
+    /**
+     * A basic unit test example to verify the user count is 50.
+     * the dd($userCount) can be used to verify the count, and the
+     * asserts verify the userCount is an integer and the last assert
+     * utilizes a less than or equal test condition.
+     *
+     * @return void
+     */
+    public function testUserCount()
+    {
+        $users = User::All();
+
+        $userCount = $users->count();
+
+        //dd($userCount);
+
+        $this->assertIsInt($userCount);
+        $this->assertLessThanOrEqual(50,$userCount);
+    }
+}
